53031e1c0a51a8dc64d44f40bdd939269c494bcc
Charity012 <sammycharity@gmail.com>
2022-03-12 15:15:02 -0800

    101quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - prints the alphabet in lowercase,
- * followed by a new line
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char ch;
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		putchar(ch);
-	}
-	putchar('\n');
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
